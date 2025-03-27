f6aa16a7f1819041ed745c8ca9b47317e636f8a7
Feuerstein Flurin <ffeuerstein@hamilton.ch>
2025-03-27 13:52:22 +0100

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod models;
-
+use anyhow::Result;
 use clap::Parser;
+use models::{Browser, Configuration, LinkPattern, MatchedPattern};
+use std::{fs, slice::Iter};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -8,8 +10,32 @@ struct Args {
     link: String,
 }
 
-fn main() {
+fn main() -> Result<()> {
+    let path = "Configuration.json";
+    let data = fs::read_to_string(path)?;
+    let configuration: Configuration = serde_json::from_str(&data)?;
+
     let args = Args::parse();
 
     println!("Hello {}!", args.link);
+
+    Ok(())
+}
+
+pub fn match_patterns(patterns: Vec<LinkPattern>, browsers: Vec<Browser>) -> Vec<MatchedPattern> {
+    patterns
+        .iter()
+        .map(|d| {
+            let browser = browsers
+                .iter()
+                .find(|browser| browser.name == d.browser)
+                .map(|d| d.to_owned());
+
+            return MatchedPattern {
+                browser,
+                pattern: d.pattern.clone(),
+                pattern_type: d.pattern_type.clone(),
+            };
+        })
+        .collect()
 }
--- src/models.rs
+++ src/models.rs
@@ -2,18 +2,21 @@ use serde::Deserialize;
 use std::path::PathBuf;
 
 #[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct Configuration {
     pub browsers: Vec<Browser>,
     pub link_patterns: Vec<LinkPattern>,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
 pub struct Browser {
     pub name: String,
     pub path: PathBuf,
 }
 
 #[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct LinkPattern {
     pub pattern_type: String,
     pub pattern: String,
@@ -23,4 +26,10 @@ pub struct LinkPattern {
 #[derive(Deserialize, Debug)]
 pub enum PatternType {
     StartsWith,
+}
+
+pub struct MatchedPattern {
+    pub pattern_type: String,
+    pub pattern: String,
+    pub browser: Option<Browser>,
 }
