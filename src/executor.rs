37275714e45f9d817834a1f4172d08a8aa73c3e4
Feuerstein Flurin <ffeuerstein@hamilton.ch>
2025-06-17 11:16:22 +0200

    some cleanup

--- src/executor.rs
+++ src/executor.rs
@@ -5,12 +5,26 @@ use crate::models::Browser;
 pub fn forward_to_browser(link: &str, browser: Option<Browser>) -> anyhow::Result<()> {
     let _ = match browser {
         Some(browser) => {
-            info!("Opening link in '{}'", browser.name);
+            info!("Opening link '{}' in '{}'", shorten(link), browser.name);
 
             open::with(link, browser.path)
         }
-        None => open::that(link),
+        None => {
+            info!("Opening link '{}' in default browser", shorten(link));
+
+            open::that(link)
+        }
     };
 
     Ok(())
+}
+
+fn shorten(string: &str) -> String {
+    if string.len() < 50 {
+        return string.into();
+    }
+
+    let mut shortened = String::from(&string[0..47]);
+    shortened.push_str("...");
+    shortened
 }
